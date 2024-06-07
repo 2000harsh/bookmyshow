ee856f80b8180b92d6add798d1eb259ed945dddb
harsh7harsh <harshbhatti1989@gmail.com>
2024-06-07 10:37:21 +0530

    ...

--- src/common/Header.js
+++ src/common/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand className="navtext" href="/">
-            BookMyShow
+            BookMyShow part
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
